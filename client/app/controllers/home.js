0a7bd7b719893c97dae0e0da316f5cf6757ea579
Curtis Badke <curtis@badkid.ca>
2013-10-13 20:46:09 -0600

    Fix controller whitespace

--- client/app/controllers/home.js
+++ client/app/controllers/home.js
@@ -1,21 +1,16 @@
 'use strict';
 
-navigator.getMedia = ( navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
+navigator.getMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia );
 
 angular.module('lookingGlass').controller('HomeCtrl', ['$scope', '$resource',
-   function($scope, $resource){
+    function($scope, $resource){
 
-       $scope.localSource = "blah";
-      navigator.getMedia(
-          {video:true, audio:true},
-          function(localMediaStream) {
-              $scope.localSource = window.URL.createObjectURL(localMediaStream);
-              $scope.$apply();
-          },
-          function(err){
-          });
-
-   }]); 
+        navigator.getMedia(
+            {video:true, audio:true},
+            function(localMediaStream) {
+                $scope.localSource = window.URL.createObjectURL(localMediaStream);
+                $scope.$apply();
+            },
+            function(err){
+            });
+    }]); 
