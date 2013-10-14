4985fd199a38b55fcd0a7e0182c1142ce97280ae
Curtis Badke <curtis@badkid.ca>
2013-10-14 13:20:01 -0600

    If no room in URL, redirect to a randomly named room.

--- client/app/controllers/home.js
+++ client/app/controllers/home.js
@@ -2,8 +2,25 @@
 
 navigator.getMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia );
 
-angular.module('lookingGlass').controller('HomeCtrl', ['$scope', '$resource',
-    function($scope, $resource){
+var genRoomName = function() {
+    var text = "";
+    var length = 10;
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    for( var i=0; i < length; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+};
+
+angular.module('lookingGlass').controller('HomeCtrl', ['$scope', '$resource', '$location', '$routeParams',
+    function($scope, $resource, $location, $routeParams){
+
+        if ($routeParams.room === null || $routeParams.room === undefined || $routeParams.room === "") {
+            var newRoomName = ($location.path() + '/' + genRoomName()).replace('//', '/');
+            $location.path(newRoomName);
+            return;
+        }
 
         navigator.getMedia(
             {video:true, audio:true},
