6d6d5f410c404417377e836c08816bf520dbe832
Curtis Badke <curtis@badkid.ca>
2013-11-12 06:14:57 -0700

    Extract getting local media stream into service.

--- client/app/controllers/home.js
+++ client/app/controllers/home.js
@@ -1,7 +1,5 @@
 'use strict';
 
-navigator.getMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia );
-
 var genRoomName = function() {
     var text = "";
     var length = 10;
@@ -14,7 +12,7 @@ var genRoomName = function() {
 };
 
 angular.module('lookingGlass').controller('HomeCtrl',
-    function($scope, $location, $routeParams, $window, room){
+    function($scope, $location, $routeParams, room, video){
 
         if ($routeParams.room === null || $routeParams.room === undefined || $routeParams.room === "") {
             var newRoomName = ($location.path() + '/' + genRoomName()).replace('//', '/');
@@ -25,33 +23,19 @@ angular.module('lookingGlass').controller('HomeCtrl',
         var id = genRoomName();
         $scope.peers = [];
         $scope.name = $routeParams.name;
+        $scope.localSource = video.stream;
+
+        video.stream.then( function(data) {
+            room.join($routeParams.room, id, $routeParams.name);
+        });
 
         room.onNewPeers( function(peers){
             $scope.peers = peers.filter( function (p) {
                 return p.id !== id;
             }).map( function(p) {
-                p.remoteSource = $scope.localSource;
+                p.remoteSource = video.stream;
                 return p;
             });
             $scope.$apply();
         });
-
-        navigator.getMedia(
-            {
-                audio: true,
-                video: {
-                    mandatory: {
-                        maxWidth: 640,
-                        maxHeight: 480
-                    }
-                },
-                optional: {}
-            },
-            function(localMediaStream) {
-                $scope.localSource = $window.URL.createObjectURL(localMediaStream);
-                $scope.$apply();
-
-                room.join($routeParams.room, id, $routeParams.name);
-            },
-            function(err){});
-        });
+    });
--- client/app/services/room.js
+++ client/app/services/room.js
@@ -1,8 +1,39 @@
 'use strict'
 
+navigator.getMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia );
+
 angular.module('lookingGlass').constant('socketio', io);
 
-angular.module('lookingGlass').factory('room', function(socketio) {
+angular.module('lookingGlass').factory('video', function($rootScope, $window, $q) {
+
+        var deferredStream = $q.defer();
+
+        navigator.getMedia(
+            {
+                audio: true,
+                video: {
+                    mandatory: {
+                        maxWidth: 640,
+                        maxHeight: 480
+                    }
+                },
+                optional: {}
+            },
+            function(localMediaStream) {
+                deferredStream.resolve($window.URL.createObjectURL(localMediaStream));
+                $rootScope.$apply(); //this feels hacky. is there a better way?
+            },
+            function(err){
+                deferredStream.reject(err);
+            }
+        );
+
+        return {
+            stream : deferredStream.promise
+        };
+});
+
+angular.module('lookingGlass').factory('room', function(socketio, video) {
     var socket = socketio.connect('/');
     var peerObservers = [];
 
