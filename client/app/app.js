96c32ba527f3c0ffce8d04ca297685e7768b3be7
Curtis Badke <curtis@badkid.ca>
2013-11-13 17:59:09 -0700

    Rip out bower, upgrade angular to 1.2.0

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-angular.module('lookingGlass', ['ngResource'])
+angular.module('lookingGlass', ['ngResource', 'ngRoute'])
 .config(['$routeProvider', function($routeProvider){
 	$routeProvider
 		.when('/:room/:name', {
